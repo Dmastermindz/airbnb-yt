2fecb685a01928f035673b591127192b095dadc9
Dmastermindz <cclarke98@gmail.com>
2023-03-25 09:28:26 +0500

    finalfix

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -5,13 +5,13 @@ import { StarIcon } from "@heroicons/react/solid"
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="flex-col md:flex-row border-2 hover:scale-[1.03] active:scale-[1] hover:shadow-lg shadow-slate-300 ml-8 mt-2 mb-8 rounded-xl transition transform durataion-250 cursor-pointer ease-out" >
+    <div className="flex-row flex-nowrap sm:flex-row border-2 hover:scale-[1.03] active:scale-[1] hover:shadow-lg shadow-slate-300 ml-8 mt-2 mb-8 rounded-xl transition transform durataion-250 cursor-pointer ease-out" >
 
-        <div className="relative flex-shrink-0 h-[180px] w-[340px] md:w-[300px] md:h-[200px]">
+        <div className="flex-row relative flex-shrink-0 h-[180px] w-[340px] md:w-[580px] md:h-[200px]">
           <Image className="rounded-tl-lg rounded-tr-lg md:rounded-tl-lg md:rounded-bl-lg" src={img} layout="fill" objectFit='cover' />
         </div>
        
-        <div className="flex w-[340px] md:w-[500px] flex-col flex-grow pl-5">
+        <div className="flex w-[340px] sm:flex-roww-[340px] md:w-[578px] flex-col flex-grow pl-5">
             <div className="flex justify-between text-gray-500 pt-4">
                 <p>{location}</p>
                 <HeartIcon className='h-7 cursor-pointer flex-shrink-0 pr-2 -mt-2'/>
@@ -28,8 +28,8 @@ function InfoCard({ img, location, title, description, star, price, total }) {
                     {star}
                 </p>
 
-                <div className="mr-4 lg:mr-8">
-                    <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+                <div className="mt-4 md:mt-2 mr-4 md:mr-4">
+                    <p className="text-lg font-semibold pb-1 md:pb-2 lg:text-2xl">{price}</p>
                     <p className="text-right pb-4 font-extralight">{total}</p>
                 </div>
             </div>
